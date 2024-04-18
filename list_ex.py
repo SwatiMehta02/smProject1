bbdb8db6ad4864162410f0e6670c24118de45e97
rishav9966 <39216235+rishav9966@users.noreply.github.com>
2024-04-18 19:46:37 +0530

    18-Apr-2024

---
+++ list_ex.py
@@ -0,0 +1,38 @@
+list1 = [1, 2, 3, '4']  # len = 4
+list2 = list()  # [] : len = 0
+
+# length of list
+print(len(list1))
+print(len(list2))
+
+list1.append(5)  # [1,2,3,4,5]
+list1.append(['hello', 5, 7.6])  # [1,2,3,4,5, ['hello', 5, 7.6]]
+
+# to find the index
+print(list1)
+# print(list1.index(100))
+list2 = [1, 2, 3, 4, 5, 1, 3, 6, 9, 3, 1, 2, 1, 1, 5]
+print(list2.index(3))
+print(list2.index(3, 4))
+# print(list2.index(3, 4, 5))
+
+# remove element
+list2.remove(3)
+print(list2)
+
+# pop element
+list2.pop()
+print(list2)
+
+print(list2.pop(4))
+print(list2)
+
+# sort list
+# list2.sort()
+list2.sort(reverse=True)
+print(list2)
+
+# count number of
+print(list2.count(1))
+
+
---
+++ main.py
@@ -0,0 +1,32 @@
+# x = 1 # type -> int
+# print(type(x))
+#
+#
+# x = False
+#
+# # print(x)
+
+# check if x is int
+# print(isinstance(x, int))
+# print(isinstance(x, float))
+# print(isinstance(x, bool))
+
+y = 4.5
+# print(isinstance(y, float))
+# print(isinstance(y, int))
+# print(isinstance(y, str))
+# print(isinstance(y, bool))
+#
+
+
+# z = "Swati"
+# print(isinstance(z, str))
+# print(isinstance(z, int))
+# print(isinstance(z, float))
+# print(isinstance(z, bool))
+# print(isinstance(z, bytes))
+#
+
+a = b'swati'
+print(isinstance(a, bytes))
+print(isinstance(a, bytearray))
