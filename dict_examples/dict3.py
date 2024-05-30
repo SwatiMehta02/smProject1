2267ec43057a0f25349777c7c92241c9abaaacfc
Rishav Anurag <itsrishavanurag@gmail.com>
2024-05-31 01:23:32 +0530

    dictionary practice 1

--- dict_examples/dict3.py
+++ dict_examples/dict3.py
@@ -7,11 +7,29 @@ employee = {
         "email": "itsswatimehta@gmail.com",
         "is_active": True
     },
-    179682: {
+    1796682: {
         "name": ("rishav", "anurag"),
         "designation": "se",
         "skills": ["python", "openshift"],
         "email": "its@gmail.com",
         "is_active": False
+    },
+    1234598: {
+        "designation": "se",
+        "skills": ["python", "java"],
+        "email": "its2@gmail.com",
+        "is_active": False,
+        "fname": "",
+        "lname": "",
+        "name": ("Gaurav", "Bhadra")
     }
 }
+for emp_id in employee:
+    fname = employee.get(emp_id).get("fname", "")
+    lname = employee.get(emp_id).get("lname", "")
+    full_name = fname + " " + lname
+    if full_name == " ":
+        full_name = employee.get(emp_id).get("name")[0] + " " + employee.get(emp_id).get("name")[1]
+    print(emp_id, full_name)
+
+    # print(emp_id,employee.get(emp_id).get("fname",))
--- dict_examples/dict_utils.py
+++ dict_examples/dict_utils.py
@@ -1,21 +1,37 @@
 room_details = {
-    "block":"J",
-    "number":12,
-    "flat":1004,
-    "rooms":None
+    "block": "J",
+    "number": 12,
+    "flat": 1004,
+    "rooms": None
 }
+
+print(room_details)
+
+room_details["furnished"] = True
+room_details["rooms"] = 2
+room_details.update({"rooms": 1, "gate": 1})
+# delete element from dictionary
+del room_details["number"]
+print(room_details)
+
+# # in operator
+# print(12 in room_details)
+#
+# for i in room_details:
+#     print(i, ":", room_details[i])
+
+# print(room_details.get("block"))
 
 # dict1 =dict()
 # print(dict1)
 
 # print(type(room_details.keys()))
 
-for i in room_details.keys():
-    print(i,":",room_details.get(i))
+# for i in room_details.keys():
+#     print(i,":",room_details.get(i))
 
-print(room_details.values())
-print(room_details.items())
+# print(room_details.values())
+# print(room_details.items())
 
-for i in room_details.items():
-    print(i[0], i[1])
-
+# for i in room_details.items():
+# print(i[0], i[1])
