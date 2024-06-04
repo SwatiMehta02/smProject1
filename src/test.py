ca277b12070284ce32b804946fedf819dece34e5
Rishav Anurag <itsrishavanurag@gmail.com>
2024-06-05 00:27:29 +0530

    Test file

--- src/test.py
+++ src/test.py
@@ -2,3 +2,27 @@ num = 2
 while num < 50:
     num = num + 2
     print(num)
+
+
+while (..............)
+
+x = True
+while x:
+
+
+a = "swati"
+while -1:                         while 1
+    # break below this
+    a = False
+
+
+while 0:
+
+
+
+x=5
+y=5
+if str(x)+str(y)=='10':
+    print("True part")
+else:
+    print("false part")
